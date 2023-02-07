4bc2b03d66dc680bb8f2f7438fab3fc7d5a55761
namethepricellc <101841832+namethepricellc@users.noreply.github.com>
2023-02-07 04:55:48 +0000

    yooooo

---
+++ client/src/login.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function registerUser(event) {
+    event.preventDefault()
+
+    const response = await fetch('https://namethepricellc-redesigned-space-5796qvppr5j37x4p-1337.preview.app.github.dev/api/users/register', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginUser}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          placeholder="Password"
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -2,19 +2,38 @@ const express = require('express')
 const app = express ()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('{MONGO_CONNECTION_STRING}')
+mongoose.connect('mongodb+srv://User1:PW123456@tickpick.evul5ee.mongodb.net/test')
 
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     try {
-        
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json({ status: 'ok'})
     } catch(err) {
-
+        res.json({ status: 'error', error: 'Duplicate email'})
     }
+})
+
+app.post('/api/login', async (req, res) => {
+    const User = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+    })
+    if (user) {
+        return res.json({ status: 'ok', user: true})
+    } else {
+        return res.json({ status: 'error', user: false})
+    }
+
     res.json({ status: 'ok'})
 })
 
